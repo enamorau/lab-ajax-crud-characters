81d4abd74e54bb260d47251191c085fbcc62dfa9
Marouane <nmraeuo@gmail.com>
2019-01-15 21:31:22 +0100

    KINDA OK THANKS TO LUCAS

--- starter-code/public/javascript/APIHandler.js
+++ starter-code/public/javascript/APIHandler.js
@@ -1,25 +1,30 @@
 class APIHandler {
   constructor (baseUrl) {
     this.BASE_URL = baseUrl;
+    this.axios = axios.create({
+      baseUrl: this.BASE_URL
+    }
+    )
   }
 
-  getFullList () {
 
+  getFullList () {
+    return axios.get("http://localhost:8000/characters")
   }
 
   getOneRegister () {
-
+    this.axios.get("/characters/:id")
   }
 
   createOneRegister () {
-
+    this.axios.post("/localhost:8000/characters/:id")
   }
 
   updateOneRegister () {
-
+    this.axios.patch("/localhost:8000/characters/:id")
   }
 
   deleteOneRegister () {
-
+    this.axios.delete("/localhost:8000/characters/:id")
   }
 }
--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,23 +1,50 @@
 const charactersAPI = new APIHandler("http://localhost:8000")
 
-$(document).ready( () => {
-  document.getElementById('fetch-all').onclick = function(){
+$(document).ready(() => {
+  document.getElementById('fetch-all').onclick = function () {
+
+    charactersAPI.getFullList()
+      .then(response => {
+        console.log('post successful and the response is: ', response);
+
+        const container = $(".characters-container")
+
+        response.data.forEach(element => {
+
+          const characterInfoDiv = $(
+            
+      `<div class="character-info">
+       
+                <div class="name">name: ${element.name}</div>
+                <div class="occupation">occupation: ${element.occupation}</div>
+                <div class="weapon">weapon: ${element.weapon}</div>
+       
+       </div>`
+       )
+
+          container.append(characterInfoDiv)
+
+        });
+
+      })
+      .catch(error => {
+        console.log('Oh No! Error is: ', error);
+      })
+  }
+
+  document.getElementById('fetch-one').onclick = function () {
 
   }
-  
-  document.getElementById('fetch-one').onclick = function(){
-    
+
+  document.getElementById('delete-one').onclick = function () {
+
   }
-  
-  document.getElementById('delete-one').onclick = function(){
-        
+
+  document.getElementById('edit-character-form').onsubmit = function () {
+
   }
-  
-  document.getElementById('edit-character-form').onsubmit = function(){
-            
-  }
-  
-  document.getElementById('new-character-form').onsubmit = function(){
-                
+
+  document.getElementById('new-character-form').onsubmit = function () {
+
   }
 })
